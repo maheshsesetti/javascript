8d0666b613fb5605c086ba11014679773555b319
Mahesh0267 <106401664+Mahesh0267@users.noreply.github.com>
2022-07-15 19:17:27 +0530

    javascript fundamentals -1

---
+++ 01-Fundamentals-part-1/script.js
@@ -0,0 +1,88 @@
+/*let js = 'amazing';
+ console.log(40+15-6);
+
+ console.log('mahesh');
+ console.log(23);
+
+ let firstName = "mahesh";
+
+ console.log(firstName);
+ console.log(firstName);
+ console.log(firstName);
+
+ let PI = 3.1415;
+
+
+//datatypes
+ true
+ let javasriptIsFun = true;
+ console.log(typeof true);
+ console.log(typeof javasriptIsFun);
+ console.log(typeof 23);
+ console.log(typeof 'mahesh');
+
+ //dynamic
+ javasriptIsFun ='YES!'
+ console.log(typeof javasriptIsFun);
+
+ let year;
+ console.log(typeof year);
+
+ year = 1995;
+
+ console.log(typeof year);
+
+ console.log(typeof null); //object
+
+ //var vs let vs const
+ let age = 30;
+ age = 31;
+
+ const birthYear = 1991;
+//  birthYear = 1990;
+
+//  const job;
+
+var job = 'programer';
+job = 'teacher';
+
+
+const ageMahesh = 2022 - 1995;
+const ageYash = 2022 - 1993;
+
+console.log(ageMahesh,ageYash);
+const firstName = 'mahesh';
+const bithYear = 1995;
+const year = 2037;
+const maheshNew = `I'm ${firstName}, a ${year - bithYear}`;
+console.log(maheshNew);
+
+console.log(23);
+console.log('Hello' - '5');
+console.log('23'-'10'+'5');
+
+//== vs === 
+
+// === is type coversion
+const age =18;
+
+if( age === 18) console.log('you just became a adult');
+
+const num = '18';
+
+if(age === 18) console.log('strict');
+if(age == 18) console.log('loose');
+
+const favorite = prompt("what is favorite number?");
+console.log(favorite);
+
+if(favorite === 23){ // '23' === 23 -->false
+        console.log('Cool!23');
+}
+
+if(Number(favorite) === 23){ // 23 === 23 -->true
+    console.log('Cool!23 Number Type');
+}
+
+if(favorite !== 23)console.log('Why not 23?');*/
+
